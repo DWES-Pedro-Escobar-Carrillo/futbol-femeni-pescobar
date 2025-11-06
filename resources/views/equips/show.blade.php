a0376bc51c90ee93633e396192d43e5fc815d419
Pedro <pedrocorreo@gmail.com>
2025-11-06 17:21:38 +0100

    Fin de todo

--- resources/views/equips/show.blade.php
+++ resources/views/equips/show.blade.php
@@ -3,4 +3,8 @@
 
 @section('content')
 <x-equip :nom="$equip['nom']" :estadi="$equip['estadi']" :titols="$equip['titols']"/>
+
+<p class="mt-4">
+    <a href="{{ route('estadis.index') }}" class="bg-gray-300 text-black px-4 py-2 rounded">Tornar al llistat</a>
+</p>
 @endsection
