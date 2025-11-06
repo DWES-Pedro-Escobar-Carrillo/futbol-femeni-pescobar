949f4e081e649c2052db0da4fea5d7c621d58389
Pedro <pedrocorreo@gmail.com>
2025-11-06 16:42:24 +0100

    Arreglado y finalizado

--- resources/views/equips/index.blade.php
+++ resources/views/equips/index.blade.php
@@ -9,7 +9,7 @@
 @endif
 
 <p class="mb-4">
-  <a href="{{ route('equips.create') }}" class="bg-blue-600 text-white px-3 py-2 rounded">Nou equip</a>
+  <a href="{{ route('equips.create') }}" class="bg-blue-600 text-white px-3 py-2 rounded">+ Nou equip</a>
 </p>
 
 <table class="w-full border-collapse border border-gray-300">
